ec82c24c477a197683eae35eeb706a0f8ae65eba
gezi-lzq <lzqtxwd@gamil.com>
2022-01-28 01:51:46 +0800

    impl template

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vue2Editor from "vue2-editor";
 
-Vue.config.productionTip = false
+import * as serviceWorker from './serviceWorker';
+import Web3 from 'web3';
+
+Vue.use(Vue2Editor);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
