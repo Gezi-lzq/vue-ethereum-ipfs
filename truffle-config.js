c706dfaf7ba90d14eca5fd1f635de8cb75938afa
gezi-lzq <lzqtxwd@gamil.com>
2022-01-28 23:54:17 +0800

    fix

--- truffle-config.js
+++ truffle-config.js
@@ -27,7 +27,7 @@ module.exports = {
   // (未编译合约)目录
   contracts_directory: './contracts/',
   // 指定合约构建生成目录
-  contracts_build_directory: './bulid/abis/',
+  contracts_build_directory: './build/abis/',
 
   compilers: {
     // Solidity编译器设置
